0afec2123a0593456e3695614e5099b62f544ae6
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2025-07-07 16:36:54 +0200

    i fixed the single quote problem

--- ui/js/ui.js
+++ ui/js/ui.js
@@ -60,12 +60,12 @@ function makeFrame(lx, ly, rx, ry, content = "", id = "", defaultpara = "", show
         for (var key in defaultpara) {
           if (defaultpara.hasOwnProperty(key)) {
             fullurl = fullurl + key + "=" +  encodeURIComponent(defaultpara[key]) + "&";
-           }
+          }
         }
         fullurl = fullurl.slice(0, -1);
       }
 
-      $(".item" + lx + "-" + ly).html("<iframe style='height: 100%; width: 100%' name='" + id +"' id='" + id +"' src='" + fullurl + "' title='' frameBorder='0' ></iframe>");
+      $(".item" + lx + "-" + ly).html("<iframe style='height: 100%; width: 100%' name='" + id +"' id='" + id +"' src=\"" + fullurl + "\" title='' frameBorder='0' ></iframe>");
 
       if(showbutton){
         $(".item" + lx + "-" + ly).append('<button class="formbutton" type="button" onclick="sendForm(\'' + '.item' + lx + '-' + ly +'\', \'' + encodeURIComponent(id) + '\', \'' + lx  + '\', \'' + ly  + '\')">' + showbutton + '</button>')
